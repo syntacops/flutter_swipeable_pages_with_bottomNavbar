54b2186e1e439c34b1772b3b6e06f3b402f4c15a
Jay <j.benker@endlichweb.de>
2020-08-26 14:33:14 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_swipeable_pages_with_bottomNavbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_swipeable_pages_with_bottomNavbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Swipeable Pages with BottomNavBar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
